0c3c4fe9e1429457efecdca06fcabeee980a7848
elanealencar <elanealencar@gmail.com>
2025-08-03 00:10:34 -0300

    adicionando novo projeto - nikel

---
+++ README.md
@@ -0,0 +1,84 @@
+# 💰 Nikel
+
+Um projeto simples de controle de finanças pessoais desenvolvido com **HTML**, **CSS**, **JavaScript puro** e **Bootstrap**. A aplicação permite o gerenciamento de entradas e saídas financeiras de forma prática, armazenando os dados no navegador com `localStorage`.
+
+## 📸 Demonstração
+
+<img src="public/assets/images/nikel_index.png" width="400" alt="Tela de Login">
+<img src="public/assets/images/nikel_home.png" width="400" alt="Tela Home">
+<img src="public/assets/images/nikel_modal.png" width="400" alt="Tela de adicionar transações">
+
+---
+
+## 🚀 Funcionalidades
+
+- ✅ Cadastro de transações (entrada e saída)
+- ✅ Listagem de transações em uma tabela
+- ✅ Cálculo automático de saldo total
+- ✅ Tooltip no botão flutuante para adicionar transações
+- ✅ Armazenamento local usando `localStorage`
+- ✅ Validação de dados no formulário
+- ✅ Design responsivo com Bootstrap 5
+
+---
+
+## 🧠 Tecnologias Utilizadas
+
+- HTML5
+- CSS3
+- JavaScript
+- Bootstrap 5
+- LocalStorage API
+
+---
+
+## 📂 Estrutura do Projeto
+
+```bash
+📁 nikel/
+├── index.html # Tela de login
+├── home.html # Tela inicial com saldo
+├── transactions.html # Tela de controle de transações
+├── css/ # Estilos CSS
+│ └── style.css
+├── js/ # Scripts JavaScript
+│ ├── index.js
+│ ├── home.js
+│ └── transactions.js
+└── assets/ # Imagens e ícones
+```
+
+---
+
+## 🧪 Como Usar
+
+1. Clone o repositório:
+
+```bash
+git clone https://github.com/seu-usuario/nikel.git
+```
+cd nikel
+
+2. Abra o arquivo index.html no navegador (ou com Live Server no VS Code).
+
+3. Crie o login:
+Basta cadastrar um nome de usuário e senha para simular o login.
+
+4. Acesse as telas:
+
+Home: mostra o saldo total e permite adicionar entradas/saídas
+Transações: permite visualizar todas as transações
+
+
+### 📝 Melhorias Futuras
+
+🔒 Implementar autenticação real
+☁️ Salvar dados em um backend real (ex: Firebase ou Express)
+🗑️ Permitir excluir transações
+✏️ Permitir editar transações
+
+
+### 👩‍💻 Desenvolvedora
+Feito por Elane
+
+[LinkedIn](https://www.linkedin.com/in/elanealencar/)
---
+++ public/js/index.js
@@ -0,0 +1,90 @@
+
+const myModal = new bootstrap.Modal("#register-Modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+checkLogged();
+
+//Fazer login no sistema
+document.getElementById("login-form").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const email = document.getElementById("email-input").value;
+    const senha = document.getElementById("password-input").value;
+    const session = document.getElementById("session-check").checked;
+
+    const account = getAccount(email);
+
+    if(!account){
+        alert("Verifique o usuário ou a senha");
+        return;
+    }
+
+    if(account){
+        if(account.password !== senha){
+            alert("Verifique o usuário ou a senha");
+            return;
+        }
+        saveSession(email, session);
+
+        window.location.href = "home.html";
+    }
+    
+});
+
+//Criar conta
+document.getElementById("create-form").addEventListener("submit", function(e){
+    e.preventDefault();
+    
+    const email = document.getElementById("email-create-input").value;
+    const senha = document.getElementById("password-create-input").value;
+
+    if(email.length < 5){
+        alert("Preencha o campo com um e-mail válido");
+        return;
+    }
+
+    if(senha.length < 4){
+        alert("Sua senha precisa ter no mínimo 4 dígitos");
+    }
+
+    saveAccount({
+        login: email,
+        password: senha,
+        transactions: []
+    });
+    myModal.hide();
+    alert("Conta criada com sucesso");
+});
+
+function saveAccount(data){
+    localStorage.setItem(data.login, JSON.stringify(data))
+}
+
+function getAccount(key){
+    const account = localStorage.getItem(key);
+
+    if(account){
+        return JSON.parse(account);
+    }
+    return "";
+}
+
+function saveSession(data, saveSession){
+    if(saveSession){
+        localStorage.setItem("session", data);
+    }
+    
+    sessionStorage.setItem("logged", data);
+}
+
+function checkLogged(){
+    if(session){
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+    if (logged) {
+        saveSession(logged, session);
+        window.location.href = "home.html";
+    }
+}
